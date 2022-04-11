67103c276da5ef6e22451d3430b9355df68b7723
ralberoc <ruben.albero.carrillo@gmail.com>
2022-04-11 12:37:28 +0200

    Listado de usuarios sin paginar

--- src/components/users/user.component.jsx
+++ src/components/users/user.component.jsx
@@ -1,10 +1,20 @@
 import { ThemeProvider } from '@emotion/react';
-import { Container, createTheme, CssBaseline, Typography, Box } from '@mui/material';
-import { getToken, saveTokenAuth } from '../../services/login.service.jsx';
+import { Container, createTheme, CssBaseline, Typography, Box, Card, CardMedia, CardActions, CardContent, Button, CardActionArea } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { fetchUserList } from '../../services/users.service.jsx';
 
 const theme = createTheme();
 
 const UserComponent = ({loginChange}) => {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => { 
+    fetchUserList(1).then(res => {
+      console.log(res);
+      setData(res.data);
+    });
+  },[]);
 
   return (
     <ThemeProvider theme={theme}>
@@ -21,6 +31,28 @@ const UserComponent = ({loginChange}) => {
           <Typography component="h1" variant="h5">
             Usuarios
           </Typography>
+
+        {data.map((card) => (
+          <Card key={card.id} sx={{ maxWidth: 345 , backgroundColor: "#1976d2"}}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image={card.avatar}
+                alt="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div" color="#FFFFFF">
+                  {card.first_name + ' ' + card.last_name}
+                </Typography>
+                <Typography variant="body2" color="#FFFFFF">
+                  Email: {card.email}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card> 
+        ))}
+
         </Box>
       </Container>
     </ThemeProvider>
--- src/services/users.service.jsx
+++ src/services/users.service.jsx
@@ -1,13 +1,14 @@
-import { getWrapper } from '../api';
+import { getWrapper } from "../api/wrappers";
+
 
 const URL = 'https://reqres.in/api/users';
 
-// export const fetchUserList = async (pageNumber: number) => {
-//   const URLparams = `${URL}?page=${pageNumber}`;
-//   const result = await getWrapper(URLparams)
-//     .then((res) => res.json())
-//     .catch((err) => err);
-//   return result;
-// };
+export const fetchUserList = async (pageNumber) => {
+  const URLparams = `${URL}?page=${pageNumber}`;
+  const result = await getWrapper(URLparams)
+    .then((res) => res.json())
+    .catch((err) => err);
+  return result;
+};
 // eslin da un error si solamente se exporta una variable
 export const clean = {};
